2e59bdc5e58893990afbf9f78fda0ce62b44222e
flix123 <felix.knoepfle@t-online.de>
2022-11-16 21:06:43 +0100

    added ListView in Discovery
    only ListView in Discovery Segment with Example Rock Songs
    TODO: get Song titles from Server to display them instead of Example Songs

--- app/src/main/java/com/example/musicfun/SimpleDiscoveryFragment.java
+++ app/src/main/java/com/example/musicfun/SimpleDiscoveryFragment.java
@@ -2,19 +2,27 @@ package com.example.musicfun;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SimpleDiscoveryFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SimpleDiscoveryFragment extends Fragment {
+public class SimpleDiscoveryFragment extends Fragment implements AdapterView.OnItemClickListener {
 
+    ListView lvDiscovery;
+    //String[] songs;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -53,12 +61,36 @@ public class SimpleDiscoveryFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple_discovery, container, false);
+        View view = inflater.inflate(R.layout.fragment_simple_discovery, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String[] songs = {"Sweet Child O' Mine", "Kickstart My Heart","The Final Countdown",
+                "Eye of the Tiger", "Crazy Train", "Hells Bells", "Poison", "Back in Black",
+                "Paradise City", "Summer of '69", "Ace of Spades", "Welcome to the Jungle",
+                "Personal Jesus", "Panama", "Africa", "Don't stop believin'", "The Boys of Summer",
+                "Black Betty", "T.N.T", "Paranoid", "Sweet Home Alabama", "Highway to Hell"};
+
+        ListView listView = (ListView)view.findViewById(R.id.lvdiscovery);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_expandable_list_item_1,songs);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        String song = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(getActivity(), "Clicked: "+ song, Toast.LENGTH_SHORT).show();
     }
 }
