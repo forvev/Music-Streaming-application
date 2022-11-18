db2c314156831fc28f9c5ed00f970b4e235304bf
heyun <lisayang821@gmail.com>
2022-11-18 21:07:32 +0100

    1. Remove duplicate classes and layouts 2. Merged discovery layout

--- app/src/main/java/com/example/musicfun/MainActivity.java
+++ app/src/main/java/com/example/musicfun/MainActivity.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -59,15 +58,31 @@ public class MainActivity extends AppCompatActivity implements PassDataInterface
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-        */
+
+//        replaceFragment(new Discovery_Decision_Fragment());
+//        binding.navView.setOnItemSelectedListener(item -> {
+//            switch (item.getItemId()){
+//                case R.id.discovery:
+//                    replaceFragment(new Discovery_Decision_Fragment());
+//                    break;
+//                case R.id.friends:
+//                    replaceFragment(new SimpleFriendsFragment());
+//                    break;
+//                case R.id.my_music:
+//                    replaceFragment(new SimpleMyMusicFragment());
+//                    break;
+//            }
+//            return true;
+//        });
     }
 
-    private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragmentsframeLayout,fragment);
-        fragmentTransaction.commit();
-    }
+//    private void replaceFragment(Fragment fragment){
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.replace(R.id.fragmentsframeLayout,fragment);
+//        fragmentTransaction.commit();
+//    }
+
     // TODO: This function should be placed somewhere else instead of MainActivity
     // TODO: Continue playing the song after pressing the stop button
     public void playFile(View v){
@@ -100,7 +115,6 @@ public class MainActivity extends AppCompatActivity implements PassDataInterface
 
     // establish connection to server
     public void sendMsg(View v) throws JSONException {
-        TextView textView = (TextView) findViewById(R.id.text_home);
         JSONObject item = new JSONObject();
         item.put("song_name", "o_tannenbaum");
         RequestQueue ExampleRequestQueue = Volley.newRequestQueue(this);
@@ -108,12 +122,10 @@ public class MainActivity extends AppCompatActivity implements PassDataInterface
             @Override
             public void onResponse(JSONObject response) {
                 // TODO: handle response from server
-                textView.setText("Response is " + response.toString());
             }
         }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
             @Override
             public void onErrorResponse(VolleyError error) {
-                textView.setText("Error with sending get request to server " + error.getMessage());
             }
         });
         ExampleRequestQueue.add(ExampleRequest);
--- app/src/main/java/com/example/musicfun/ui/discovery/DiscoveryFragment.java
+++ app/src/main/java/com/example/musicfun/ui/discovery/DiscoveryFragment.java
@@ -12,12 +12,17 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.musicfun.DiscoveryPart.DiscoveryChartsFragment;
+import com.example.musicfun.DiscoveryPart.DiscoveryMayLikeFragment;
+import com.example.musicfun.DiscoveryPart.DiscoveryMostHeardFragment;
+import com.example.musicfun.DiscoveryPart.SimpleDiscoveryFragment;
+import com.example.musicfun.R;
 import com.example.musicfun.databinding.FragmentDiscoveryBinding;
 import com.example.musicfun.interfaces.PassDataInterface;
 import com.example.musicfun.search.SearchResultAdapter;
@@ -31,8 +36,46 @@ public class DiscoveryFragment extends Fragment implements SearchView.OnQueryTex
     SearchResultAdapter adapter;
     SearchView editsearch;
     DiscoveryViewModel discoveryViewModel;
-
     public PassDataInterface mOnInputListner;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public DiscoveryFragment() {
+        // Required empty public constructor
+    }
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Discovery_Decision_Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DiscoveryFragment newInstance(String param1, String param2) {
+        DiscoveryFragment fragment = new DiscoveryFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         discoveryViewModel = new ViewModelProvider(this).get(DiscoveryViewModel.class);
@@ -44,8 +87,6 @@ public class DiscoveryFragment extends Fragment implements SearchView.OnQueryTex
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
-        final TextView textView = binding.textHome;
-        discoveryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         // locate the ListView in fragment_discovery.xml
         listView = binding.searchList;
         // pass results to ListViewAdapter Class
@@ -69,6 +110,8 @@ public class DiscoveryFragment extends Fragment implements SearchView.OnQueryTex
                 String input = s.getSongName();
                 mOnInputListner.sendInput(input);
                 listView.setVisibility(View.INVISIBLE);
+                binding.DiscoveryNav.setVisibility(View.VISIBLE);
+                binding.discoveryChildFragment.setVisibility((View.VISIBLE));
             }
         });
         // locate the SearchView in fragment_discovery.xml
@@ -78,10 +121,31 @@ public class DiscoveryFragment extends Fragment implements SearchView.OnQueryTex
             @Override
             public void onFocusChange(View view, boolean b) {
                 listView.setVisibility(View.VISIBLE);
-                textView.setVisibility(View.INVISIBLE);
+                binding.DiscoveryNav.setVisibility(View.INVISIBLE);
+                binding.discoveryChildFragment.setVisibility((View.INVISIBLE));
             }
         });
         editsearch.setOnQueryTextListener(this);
+
+        // link to other song list fragments
+        insertNestedFragment(new SimpleDiscoveryFragment());
+        binding.DiscoveryNav.setOnItemSelectedListener(item ->{
+            switch (item.getItemId()){
+                case  R.id.new_releases:
+                    insertNestedFragment(new SimpleDiscoveryFragment());
+                    break;
+                case R.id.most_heard:
+                    insertNestedFragment(new DiscoveryMostHeardFragment());
+                    break;
+                case R.id.charts:
+                    insertNestedFragment(new DiscoveryChartsFragment());
+                    break;
+                case R.id.may_like:
+                    insertNestedFragment(new DiscoveryMayLikeFragment());
+                    break;
+            }
+            return true;
+        });
     }
 
     @Override
@@ -94,7 +158,7 @@ public class DiscoveryFragment extends Fragment implements SearchView.OnQueryTex
         String text = newText;
         adapter.filter(text);
         if(TextUtils.isEmpty(text)){
-            listView.setVisibility(View.GONE);
+            listView.setVisibility(View.INVISIBLE);
         }
         else {
             listView.setVisibility(View.VISIBLE);
@@ -111,6 +175,11 @@ public class DiscoveryFragment extends Fragment implements SearchView.OnQueryTex
             InputMethodManager manager = (InputMethodManager) getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
             manager.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
+    }
+
+    private void insertNestedFragment(Fragment childFragment) {
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.discovery_childFragment, childFragment).commit();
     }
 
     // rewrite onAttach to send item clicked in the list back to main activity
