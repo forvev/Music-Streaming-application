1d33cc0ed347208d5d17540f015d49ac866a6b9a
Yang, Heyun <lisayang821@gmail.com>
2022-12-01 16:29:44 +0100

    Update Database.java

--- app/src/main/java/com/example/musicfun/Database.java
+++ app/src/main/java/com/example/musicfun/Database.java
@@ -21,37 +21,9 @@ public class Database {
     private ArrayList<Songs> songsArrayList = new ArrayList<>();
     Context context;
 
-//    private MutableLiveData<ArrayList<Songs>> myLiveData = new MutableLiveData<>();
-
     public Database(Context context){
         this.context = context;
     }
-
-//    public ArrayList<Songs> sendMsg(String query, Context context) throws JSONException {
-//        RequestQueue requestQueue = Volley.newRequestQueue(context);
-//        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-//            @Override
-//            public void onResponse(JSONObject response) {
-//                // TODO: switch case
-//                try {
-//                    JSONArray songTitles = (JSONArray) response.get("All songs");
-//                    for (int i = 0; i < songTitles.length(); i++) {
-//                        Songs s = new Songs(songTitles.getJSONObject(i).getString("title"), songTitles.getJSONObject(i).getString("artist"), songTitles.getJSONObject(i).getInt("id"));
-//                        songsArrayList.add(s);
-//                    }
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                System.out.println("Error Response" + error.getMessage());
-//            }
-//        });
-//        requestQueue.add(request);
-//        return songsArrayList;
-//    }
 
     public void sendMsg(ServerCallBack callback, String url) {
         RequestQueue requestQueue = Volley.newRequestQueue(context);
@@ -59,7 +31,6 @@ public class Database {
             @Override
             public void onResponse(JSONObject response) {
                 callback.onSuccess(response);
-                // TODO: switch case
             }
         }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
             @Override
