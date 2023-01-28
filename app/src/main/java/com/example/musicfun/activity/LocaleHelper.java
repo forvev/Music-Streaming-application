56621bd782ad2c697d85632274ed220c779043b5
heyun <lisayang821@gmail.com>
2023-01-28 20:33:23 +0100

    additional files

---
+++ app/src/main/java/com/example/musicfun/activity/LocaleHelper.java
@@ -0,0 +1,88 @@
+package com.example.musicfun.activity;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.preference.PreferenceManager;
+
+import java.util.Locale;
+
+public class LocaleHelper {
+    private static final String SELECTED_LANGUAGE = "Locale.Helper.Selected.Language";
+
+    public static Context onAttach(Context context) {
+        String lang = getPersistedData(context, Locale.getDefault().getLanguage());
+        return setLocale(context, lang);
+    }
+
+    public static Context onAttach(Context context, String defaultLanguage) {
+        String lang = getPersistedData(context, defaultLanguage);
+        return setLocale(context, lang);
+    }
+
+    public static String getLanguage(Context context) {
+        return getPersistedData(context, Locale.getDefault().getLanguage());
+    }
+
+    private static String getPersistedData(Context context, String defaultLanguage) {
+        SharedPreferences sp = context.getSharedPreferences("login", MODE_PRIVATE);
+        return sp.getString(SELECTED_LANGUAGE, defaultLanguage);
+    }
+
+    // the method is used to set the language at runtime
+    public static Context setLocale(Context context, String language) {
+        persist(context, language);
+
+        // updating the language for devices above android nougat
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return updateResources(context, language);
+        }
+        // for devices having lower version of android os
+        return updateResourcesLegacy(context, language);
+    }
+
+    private static void persist(Context context, String language) {
+        SharedPreferences sp = context.getSharedPreferences("login", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(SELECTED_LANGUAGE, language);
+        editor.apply();
+    }
+
+    // the method is used update the language of application by creating
+    // object of inbuilt Locale class and passing language argument to it
+    @TargetApi(Build.VERSION_CODES.N)
+    private static Context updateResources(Context context, String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Configuration configuration = context.getResources().getConfiguration();
+        configuration.setLocale(locale);
+        configuration.setLayoutDirection(locale);
+
+        return context.createConfigurationContext(configuration);
+    }
+
+
+    @SuppressWarnings("deprecation")
+    private static Context updateResourcesLegacy(Context context, String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Resources resources = context.getResources();
+
+        Configuration configuration = resources.getConfiguration();
+        configuration.locale = locale;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            configuration.setLayoutDirection(locale);
+        }
+
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+
+        return context;
+    }
+}
