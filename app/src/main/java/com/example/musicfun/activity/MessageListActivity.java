f61657f5c38eb8f037c47f9a68f499d7579f4d26
heyun <lisayang821@gmail.com>
2023-01-28 21:35:55 +0100

    full screen mode for chat activity

--- app/src/main/java/com/example/musicfun/activity/MessageListActivity.java
+++ app/src/main/java/com/example/musicfun/activity/MessageListActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.widget.Toolbar;
 
@@ -26,6 +28,8 @@ public class MessageListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding = ActivityChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
